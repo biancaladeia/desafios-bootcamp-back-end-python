3a770a95032435d22fcc37d5b188b34fe236987b
biancaladeia <biancaladeia@icloud.com>
2023-08-24 14:09:17 -0300

    módulo 2: Tomada de Decisão e Estruturas de Repetição

---
+++ desafio1/modulo2/exercicio1.py
@@ -0,0 +1,10 @@
+# Faça um Programa que peça dois números e imprima o maior deles.
+
+numero1 = int(input('Digite um número: '))
+numero2 = int(input('Digite outro número: '))
+
+if numero1 > numero2:
+    print(f'O maior número é: {numero1}.')
+else: 
+    print(f'O maior número é: {numero2} ')
+
