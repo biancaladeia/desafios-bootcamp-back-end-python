46479dab1a99bc31001c8f7522e5ad16d15c9e10
biancaladeia <biancaladeia@icloud.com>
2023-08-24 13:43:23 -0300

    Exercícios: Conceitos básicos de Python

---
+++ desafio1/modulo1/exercicio2.py
@@ -0,0 +1,7 @@
+# Faça um Programa que peça dois números e imprima a soma.
+
+numero1 = int(input('Digite um número: '))
+numero2 = int(input('Digite outro número: '))
+
+soma = numero1 + numero2
+print(f'A soma dos dois números é: {soma}')
---
+++ desafio1/modulo1/exercicio4.py
@@ -0,0 +1,7 @@
+# Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+
+valor_hora = float(input('Informe o valor da sua hora de trabalho: '))
+horas_trabalhadas = float(input('Informe a quantidade de horas trabalhadas no mês: '))
+salario = valor_hora * horas_trabalhadas
+print(f'O seu salário no mês é de R$ {salario}')
+
