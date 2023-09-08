066b33637415e3c52c0d0592a5ba5aca559be249
Bianca Ladeia <138152320+biancaladeia@users.noreply.github.com>
2023-09-08 19:55:41 -0300

    exercicio 2 banco de dados

--- desafio-2/modulo-2/lista-de-exercicios.py
+++ desafio-2/modulo-2/lista-de-exercicios.py
@@ -9,3 +9,24 @@ c.execute('CREATE TABLE alunos (id INT, nome VARCHAR(100), idade INT, curso VARC
 conexao.commit()
 conexao.close()
 
+#2. Insira pelo menos 5 registros de alunos na tabela que você criou no exercício anterior.
+
+# Inserindo o primeiro aluno
+c.execute("INSERT INTO alunos VALUES (1, 'João', 20, 'Engenharia')")
+
+# Inserindo o segundo aluno
+c.execute("INSERT INTO alunos VALUES (2, 'Maria', 22, 'Medicina')")
+
+# Inserindo o terceiro aluno
+c.execute("INSERT INTO alunos VALUES (3, 'Pedro', 21, 'Direito')")
+
+# Inserindo o quarto aluno
+c.execute("INSERT INTO alunos VALUES (4, 'Ana', 19, 'Engenharia')")
+
+# Inserindo o quinto aluno
+c.execute("INSERT INTO alunos VALUES (5, 'Miguel', 23, 'Administração')")
+
+# Salvando as alterações e fechando a conexão
+conexao.commit()
+conexao.close()
+
